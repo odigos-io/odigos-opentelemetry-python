6910d89e58b01423db96e9ef92c88505fd15a010
tamirdavid1 <tamir@odigos.io>
2024-12-29 10:55:12 +0200

    chore: set protobuf version

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         "odigos-requests == 2.32.3.dev0",
         "requests == 2.32.3",
         'opentelemetry-distro==0.49b2',
+        'protobuf==5.29.2',
         'opentelemetry-exporter-otlp-proto-http==1.28.2',
         'opentelemetry-instrumentation==0.49b2',
         'opentelemetry-instrumentation-aio-pika==0.49b2',
