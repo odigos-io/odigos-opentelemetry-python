9cbc607dba067b1d7a02fe15cee54e6fe857eadb
tamirdavid1 <tamir@odigos.io>
2025-07-20 11:06:37 +0300

    chore: bump py version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="odigos-opentelemetry-python",
-    version="1.0.50",
+    version="1.0.51",
     description="Odigos Initializer for Python OpenTelemetry Components",
     author="Tamir David",
     author_email="tamir@odigos.io",
