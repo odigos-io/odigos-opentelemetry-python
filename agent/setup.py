9c89f0ffa5c91a1c54978afec5e09f709d393475
ErikNaz <erik98na@gmail.com>
2026-03-10 10:31:51 +0200

    Added infra to release python artifacts under public.ecr.aws/odigos/agents/python-community image (#159)

---
+++ agent/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+index_url = None
+
+install_requires = [
+    f"odigos-opentelemetry-python @ {index_url}" if index_url else "odigos-opentelemetry-python==1.0.66"
+]
+
+setup(
+    name="odigos-python-configurator",
+    version="1.0.66",
+    description="Odigos Configurator for Python OpenTelemetry Auto-Instrumentation",
+    author="Tamir David",
+    author_email="tamir@odigos.io",
+    packages=find_packages(include=["configurator", "configurator.*"]),
+    install_requires=install_requires,
+    python_requires=">=3.8",
+    entry_points={
+        'opentelemetry_configurator': [
+            'odigos-python-configurator = configurator:OdigosPythonConfigurator'
+        ],
+    },
+)
