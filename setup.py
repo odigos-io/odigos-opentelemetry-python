6ccb93d189b0085418f80f2c097d2508afedc144
Tamir David <tamir@odigos.io>
2026-03-24 14:18:15 +0200

    chore: move reordering (#171)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="odigos-opentelemetry-python",
-    version="1.0.69",
+    version="1.0.70",
     description="Odigos Initializer for Python OpenTelemetry Components",
     author="odigos-io",
     author_email="support@odigos.io",
